efc985b45fb18f30b1198a9e7b19c38dd60dbf35
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-07 01:03:55 +0530

    Update MaximumProductSubarray.java

--- ARRAY/MaximumProductSubarray.java
+++ ARRAY/MaximumProductSubarray.java
@@ -1,1 +1,23 @@
 
+class Solution {
+    // Function to find maximum product subarray
+    int maxProduct(int[] arr) {
+        int n=arr.length;
+        int result=Integer.MIN_VALUE;
+        int l=1;
+        int r=1;
+        for(int i=0;i<n;i++){
+            if(l==0)
+              l=1;
+            if(r==0)
+              r=1;
+              
+              
+            l*=arr[i];
+            int j=n-i-1;
+            r*=arr[j];
+            result=Math.max(l,Math.max(r,result));
+        }
+        return result;
+    }
+}
