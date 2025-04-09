108e05042ad1874168169f511362003644ea0999
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-09 23:43:35 +0530

    Update SmallestPositiveMissing.java

--- ARRAY/SmallestPositiveMissing.java
+++ ARRAY/SmallestPositiveMissing.java
@@ -1,1 +1,19 @@
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) {
+        int n=arr.length;
+        
+        boolean[]vis=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(arr[i]>0 && arr[i]<=n)
+                vis[arr[i]-1]=true;
+        }
+        for(int i=1;i<=n;i++){
+            if(!vis[i-1]){
+                return i;
+            }
+        }
+        return n+1;
+    }
+}
 
