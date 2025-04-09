f7adafd33e97b5706968242aacd98739eb1cb07d
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-09 23:49:44 +0530

    Update ImplementAtoi.java

--- string/ImplementAtoi.java
+++ string/ImplementAtoi.java
@@ -1,1 +1,26 @@
-
+class GfG {
+    static int myAtoi(String s) {
+        int sign = 1, res = 0, idx = 0;
+        while (idx < s.length() && s.charAt(idx) == ' ') {
+            idx++;
+        }
+        if (idx < s.length() && (s.charAt(idx) == '-' 
+                                 || s.charAt(idx) == '+')) {
+            if (s.charAt(idx++) == '-') {
+                sign = -1;
+            }
+        }
+        while (idx < s.length() && s.charAt(idx) >= '0' 
+               						&& s.charAt(idx) <= '9') {
+          
+            if (res > Integer.MAX_VALUE / 10 || 
+                   (res == Integer.MAX_VALUE / 10 && s.charAt(idx) - '0' > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+          
+        
+            res = 10 * res + (s.charAt(idx++) - '0');
+        }
+        return res * sign;
+    }
+}
