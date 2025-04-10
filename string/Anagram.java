56b5a6574553ed0c3ab31113c258991dcf5b8743
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-10 23:14:22 +0530

    Update Anagram.java

--- string/Anagram.java
+++ string/Anagram.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+        char[] s1Array = s1.toCharArray();
+        char[] s2Array = s2.toCharArray();
+        Arrays.sort(s1Array);
+        Arrays.sort(s2Array);
+        
+        return Arrays.equals(s1Array , s2Array);
+    }
+}
