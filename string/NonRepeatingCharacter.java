9cc516daf93f107117718de39bba98effafe7ccc
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-13 01:27:55 +0530

    Update NonRepeatingCharacter.java

--- string/NonRepeatingCharacter.java
+++ string/NonRepeatingCharacter.java
@@ -1,1 +1,17 @@
+class Solution {
+    private  static final int maxchar=26;
+    static char nonRepeatingChar(String s) {
+        int[] freq=new int[maxchar];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        for(char c:s.toCharArray()){
+            if(freq[c-'a']==1){
+                return c;
+            }
+        }
+        
+        return '$';
+    }
+}
 
