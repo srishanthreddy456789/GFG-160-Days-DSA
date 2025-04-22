2289c044952e37ce27fca8c32fe60bb3c57773e0
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-22 21:22:13 +0530

    Create Non-overlappingIntervals.js

---
+++ Sorting/Non-overlappingIntervals.js
@@ -0,0 +1,19 @@
+class Solution {
+    static int minRemoval(int intervals[][]) {
+        int cnt=0;
+        
+        
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int end=intervals[0][1];
+        
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<end){
+                cnt++;
+                end=Math.min(intervals[i][1],end);
+            }else{
+                end=intervals[i][1];
+            }
+        }
+        return cnt;
+    }
+}
