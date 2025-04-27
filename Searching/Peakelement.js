2f7c6816933c77b738e32ae078bb747630b5ab3b
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-27 15:23:58 +0530

    Update Peakelement.js

--- Searching/Peakelement.js
+++ Searching/Peakelement.js
@@ -1,1 +1,23 @@
+class Solution {
+
+    public int peakElement(int[] arr) {
+        int n=arr.length;
+        
+        for(int i=0;i<n;i++){
+            boolean left=true;
+            boolean right=true;
+            
+            if(i>0 && arr[i]<=arr[i-1]){
+                left=false;
+            }
+            if(i<n-1 && arr[i]<=arr[i+1]){
+                right=false;
+            }
+            if(left && right){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
 
