5fde24aa2d1ca7e32ee32b57755026dddf96ddcf
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-27 15:13:42 +0530

    Update SortedandRotatedMinimum.js

--- Searching/SortedandRotatedMinimum.js
+++ Searching/SortedandRotatedMinimum.js
@@ -1,1 +1,10 @@
 
+class Solution {
+    public int findMin(int[] arr) {
+        int res=arr[0];
+        for(int i=0;i<arr.length;i++){
+            res=Math.min(arr[i],res);
+        }
+        return res;
+    }
+}
