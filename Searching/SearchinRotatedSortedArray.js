fa6c42b14207082766e0446bafd634bb790b0f6b
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-04-27 15:14:10 +0530

    Update SearchinRotatedSortedArray.js

--- Searching/SearchinRotatedSortedArray.js
+++ Searching/SearchinRotatedSortedArray.js
@@ -1,1 +1,19 @@
 
+class Solution {
+    
+    int search(int[] arr, int key) {
+        
+        int i;
+        
+        for(i=0;i<arr.length;i++)
+        {
+            if(arr[i]==key)
+            {
+                return i;
+            }
+        }
+        
+            return -1;
+        
+    }
+}
