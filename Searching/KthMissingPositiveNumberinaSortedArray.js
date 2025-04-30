6284bb0915711ed4c69a2e26a841683843500f1c
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-01 01:04:56 +0530

    Update KthMissingPositiveNumberinaSortedArray.js

--- Searching/KthMissingPositiveNumberinaSortedArray.js
+++ Searching/KthMissingPositiveNumberinaSortedArray.js
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        Set<Integer>set=new HashSet<>();
+        for(int num:arr){
+            set.add(num);
+        }
+        int c=0,r=0;
+        
+        while(c<k){
+            r++;
+            if(!set.contains(r)){
+                c++;
+            }
+        }
+        return r;
+    }
+}
