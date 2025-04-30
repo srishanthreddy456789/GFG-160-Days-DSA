47052b6ff73a3571389d2319b3ed6dcbbc4f3cdb
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-01 01:04:30 +0530

    Update K-thelementoftwoArrays.js

--- Searching/K-thelementoftwoArrays.js
+++ Searching/K-thelementoftwoArrays.js
@@ -1,1 +1,18 @@
 
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        ArrayList<Integer>arr=new ArrayList<>();
+        
+        for(int i:a)
+         arr.add(i);
+         
+        for(int i:b)
+         arr.add(i);
+         
+         
+        Collections.sort(arr);
+        
+          return arr.get(k-1);
+        
+    }
+}
