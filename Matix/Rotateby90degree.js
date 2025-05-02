f10704a34b88fcb631c2eca750d5b9a4d41b910a
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-02 16:34:35 +0530

    Update Rotateby90degree.js

--- Matix/Rotateby90degree.js
+++ Matix/Rotateby90degree.js
@@ -1,1 +1,19 @@
 
+class GfG {
+    static void rotateMatrix(int[][] mat) {
+        int n = mat.length;
+        int[][] res = new int[n][n];
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[n - j - 1][i] = mat[i][j];
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(res[i], 0, mat[i], 0, n);
+        }
+    }
+
