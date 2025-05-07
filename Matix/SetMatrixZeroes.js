5c68776cb01a5ea4a9d82122cc8e766ae60fe484
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-07 23:15:46 +0530

    Update SetMatrixZeroes.js

--- Matix/SetMatrixZeroes.js
+++ Matix/SetMatrixZeroes.js
@@ -1,1 +1,25 @@
 
+class Solution {
+    public void setMatrixZeroes(int[][] mat) {
+        int n=mat.length,m=mat[0].length;
+        
+        boolean[] row= new boolean[n];
+        boolean[] col=new boolean[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]||col[j]){
+                    mat[i][j]=0;
+                }
+            }
+        }
+        
+    }
+}
