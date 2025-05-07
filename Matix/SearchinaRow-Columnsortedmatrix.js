707a76abcd0b3b827956c98b09b504c8f01cb461
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-07 23:14:24 +0530

    Update SearchinaRow-Columnsortedmatrix.js

--- Matix/SearchinaRow-Columnsortedmatrix.js
+++ Matix/SearchinaRow-Columnsortedmatrix.js
@@ -1,1 +1,14 @@
-
+class Solution {
+    public static boolean matSearch(int mat[][], int x) {
+        int n=mat.length,m=mat[0].length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==x){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
