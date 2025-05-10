b24cf2f98649b701a04391ca3b5f5f1eef49109e
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-10 22:14:05 +0530

    Update Countpairswithgivensum.java

--- Hashing/Countpairswithgivensum.java
+++ Hashing/Countpairswithgivensum.java
@@ -1,1 +1,15 @@
+class Solution {
 
+    int countPairs(int arr[], int target) {
+        Map<Integer,Integer>freq=new HashMap<>();
+        int c=0;
+        for(int i=0;i<arr.length;i++){
+            if(freq.containsKey(target-arr[i])){
+                c+=freq.get(target-arr[i]);
+            }
+            
+            freq.put(arr[i],freq.getOrDefault(arr[i],0)+1);
+        }
+        return c;
+        }
+}
