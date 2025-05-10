b2c61d63ceb655db226c4c429aa76f0ae2f9f799
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-10 22:14:36 +0530

    Update TwoSum-PairwithGivenSumTwoSum-PairwithGivenSum.java

--- Hashing/TwoSum-PairwithGivenSumTwoSum-PairwithGivenSum.java
+++ Hashing/TwoSum-PairwithGivenSumTwoSum-PairwithGivenSum.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        Arrays.sort(arr);
+        int left=0,right=arr.length-1;
+        
+        while(left<right){
+            int sum=arr[left]+arr[right];
+            if(sum==target){
+                return true;
+            }else if(sum<target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return false;
+        
+    }
+}
