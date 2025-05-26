60cc076dc2645a1ba716a83089a519cde5c81b6d
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-26 18:39:45 +0530

    Update 0-1KnapsackProblem.java

--- Dynamic Programming/0-1KnapsackProblem.java
+++ Dynamic Programming/0-1KnapsackProblem.java
@@ -1,1 +1,23 @@
+class GfG {
+    static int lisEndingAtIdx(int[] arr, int idx) {
+      
+        // Base case
+        if (idx == 0)
+            return 1;
+
+      
+        int mx = 1;
+        for (int prev = 0; prev < idx; prev++)
+            if (arr[prev] < arr[idx])
+                mx = Math.max(mx, lisEndingAtIdx(arr, prev) + 1);
+        return mx;
+    }
+
+    static int lis(int[] arr) {
+        int n = arr.length;
+        int res = 1;
+        for (int idx = 1; idx < n; idx++)
+            res = Math.max(res, lisEndingAtIdx(arr, idx));
+        return res;
+    }
 
