17891c57a452cc6326b45eedc58f8f581bebfd7a
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-27 21:25:50 +0530

    Update LongestPalindromicSubsequence.java

--- Dynamic Programming/LongestPalindromicSubsequence.java
+++ Dynamic Programming/LongestPalindromicSubsequence.java
@@ -1,1 +1,24 @@
+class GfG {
+
+
+    static int lps(String s, int low, int high) {
+
+        
+        if (low > high) return 0;
+
+        
+        if (low == high)
+            return 1;
+
+        
+        if (s.charAt(low) == s.charAt(high))
+            return lps(s, low + 1, high - 1) + 2;
+
+       
+        return Math.max(lps(s, low, high - 1), lps(s, low + 1, high));
+    }
+
+    static int longestPalinSubseq(String s) {
+        return lps(s, 0, s.length() - 1);
+    }
 
