ab823462a1c4e2a53b7e35f58dd97bba4514ab33
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-27 21:28:39 +0530

    Update PalindromeSubStrings.java

--- Dynamic Programming/PalindromeSubStrings.java
+++ Dynamic Programming/PalindromeSubStrings.java
@@ -1,1 +1,28 @@
+class GfG {
+    
+    
+    static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
 
+    static int countPS(String s) {
+        int n = s.length();
+
+       
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+          
+                
+                if (isPalindrome(s, i, j)) 
+                    res++;  
+            }
+        }
+
+        return res;
+    }
