730f9795c3c296c2257e4df867f948b0c6cfdce4
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-27 21:25:14 +0530

    Update LongestCommonSubsequence.java

--- Dynamic Programming/LongestCommonSubsequence.java
+++ Dynamic Programming/LongestCommonSubsequence.java
@@ -1,1 +1,23 @@
+class GfG {
 
+
+    static int lcsRec(String s1, String s2, int m, int n) {
+
+        
+        if (m == 0 || n == 0)
+            return 0;
+
+        
+        if (s1.charAt(m - 1) == s2.charAt(n - 1))
+
+  
+            return 1 + lcsRec(s1, s2, m - 1, n - 1);
+
+        else
+           
+            return Math.max(lcsRec(s1, s2, m, n - 1), lcsRec(s1, s2, m - 1, n));
+    }
+    static int lcs(String s1,String s2){
+        int m = s1.length(), n = s2.length();
+        return lcsRec(s1,s2,m,n);
+    }
