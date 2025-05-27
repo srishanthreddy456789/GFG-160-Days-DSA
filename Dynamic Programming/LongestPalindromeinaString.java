c2c8d3a3980a9a34ed6bf3f61189740d74cb26d8
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-27 21:27:42 +0530

    Update LongestPalindromeinaString.java

--- Dynamic Programming/LongestPalindromeinaString.java
+++ Dynamic Programming/LongestPalindromeinaString.java
@@ -10,4 +10,26 @@ class GfG {
         }
         return true;
     }
+ static String longestPalindrome(String s) {
+
+     
+        int n = s.length();
+
+        
+        int maxLen = 1, start = 0;
+
+    
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                
+                if (checkPal(s, i, j) && (j - i + 1) > maxLen) {
+                    start = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
 
