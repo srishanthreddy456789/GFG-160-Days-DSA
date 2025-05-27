54ebaa394405eced7f81924c2cad4c031352a8a7
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-27 21:26:23 +0530

    Update LongestPalindromeinaString.java

--- Dynamic Programming/LongestPalindromeinaString.java
+++ Dynamic Programming/LongestPalindromeinaString.java
@@ -1,1 +1,13 @@
+class GfG {
+
+    
+    static boolean checkPal(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low) != s.charAt(high))
+                return false;
+            low++;
+            high--;
+        }
+        return true;
+    }
 
