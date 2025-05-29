b6c79ee9a704574be1a450599aeb737afb790e0f
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-30 01:17:26 +0530

    Update CoinChange.java

--- Dynamic Programming/CoinChange.java
+++ Dynamic Programming/CoinChange.java
@@ -1,1 +1,19 @@
+class GfG {
+
+    static int countRecur(int[] coins, int n, int sum) {
+
+        
+        if (sum == 0) return 1;
+
+        
+        if (sum < 0 || n == 0) return 0;
+
+        
+        return countRecur(coins, n, sum - coins[n - 1]) +
+                countRecur(coins, n - 1, sum);
+    }
+
+    static int count(int[] coins, int sum) {
+        return countRecur(coins, coins.length, sum);
+    }
 
