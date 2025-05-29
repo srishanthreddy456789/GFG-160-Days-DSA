c13bc2ecda128e965b8df3b8204bbf23ad7bca87
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-30 01:24:56 +0530

    Update Stock BuyandSell–MaxKTransactionsAllowed.java

--- Dynamic Programming/Stock BuyandSell–MaxKTransactionsAllowed.java
+++ Dynamic Programming/Stock BuyandSell–MaxKTransactionsAllowed.java
@@ -1,1 +1,39 @@
+class GfG {
+    
+    static int maxProfitUtil(int i, int k,
+                         int buy,int[] prices){
+
+        
+        if (k <= 0 || i >= prices.length)
+            return 0;
+
+        int result = 0, profit = 0;
+
+        
+        if (buy == 1){
+          
+           
+            profit = maxProfitUtil(i + 1, k, 0,
+                           prices) - prices[i];
+            result = Math.max(result, profit);
+        } else{
+          
+          
+            profit = prices[i] + 
+              maxProfitUtil(i + 1,k - 1,1,prices);
+            result = Math.max(result, profit);
+        }
+
+       
+        profit = maxProfitUtil(i + 1, k, buy,
+                               prices);
+        return Math.max(result, profit);
+    }
+
+    
+    static int maxProfit(int[] prices, int k){
+      
+        
+        return maxProfitUtil(0, k, 1, prices);
+    }
 
