b92abc6c89bcd968e7972d83fc4d2e9ac11a0618
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-30 01:26:41 +0530

    Update WordBreak.java

--- Dynamic Programming/WordBreak.java
+++ Dynamic Programming/WordBreak.java
@@ -1,1 +1,25 @@
+class GfG {
+    static boolean wordBreakRec(int i, String s,
+                                String[] dictionary)
+    {
+        if (i == s.length())
+            return true;
 
+        String prefix = "";
+
+        for (int j = i; j < s.length(); j++) {
+            prefix += s.charAt(j);
+
+            
+            if (Arrays.asList(dictionary).contains(prefix)
+                && wordBreakRec(j + 1, s, dictionary)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean wordBreak(String s, String[] dictionary)
+    {
+        return wordBreakRec(0, s, dictionary);
+    }
