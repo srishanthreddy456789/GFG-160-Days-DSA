f61df9cc09604e34124edf20b863ba2945ed5986
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-30 01:23:37 +0530

    Update SticklerThiefII.java

--- Dynamic Programming/SticklerThiefII.java
+++ Dynamic Programming/SticklerThiefII.java
@@ -1,1 +1,27 @@
+class GfG {
+    static int maxLootRec(int[] arr, int n, int[] memo) {
+        if (n <= 0) return 0;
+        if (n == 1) return arr[0];
+
+   
+        if (memo[n] != -1) return memo[n];
+
+        int pick = arr[n - 1] + maxLootRec(arr, n - 2, memo);
+        int notPick = maxLootRec(arr, n - 1, memo);
+
+       
+        memo[n] = Math.max(pick, notPick);
+        return memo[n];
+    }
+
+
+    static int findMaxSum(int[] arr) {
+        int n = arr.length;
+      
+
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+
+        return maxLootRec(arr, n, memo);
+    }
 
