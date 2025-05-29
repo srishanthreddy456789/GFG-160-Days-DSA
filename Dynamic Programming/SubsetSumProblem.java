fae75463a7f7f34ef065e8e4ac775ef7bdd132b4
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-30 01:25:31 +0530

    Update SubsetSumProblem.java

--- Dynamic Programming/SubsetSumProblem.java
+++ Dynamic Programming/SubsetSumProblem.java
@@ -1,1 +1,27 @@
+class GfG {
+
+    
+   	static boolean isSubsetSumRec(int[] arr, int n, int sum) {
+      
+        
+        if (sum == 0) {
+            return true;
+        }
+        if (n == 0) {
+            return false;
+        }
+
+        
+        if (arr[n - 1] > sum) {
+            return isSubsetSumRec(arr, n - 1, sum);
+        }
+
+        
+        return isSubsetSumRec(arr, n - 1, sum) || 
+               isSubsetSumRec(arr, n - 1, sum - arr[n - 1]);
+    }
+
+    static boolean isSubsetSum(int[] arr, int sum) {
+        return isSubsetSumRec(arr, arr.length, sum);
+    }
 
