689f98facaab909b5002906a1cb2f5cb1c2aaa7e
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-30 01:16:59 +0530

    Update CoinChange(Minimum Coins).java

--- Dynamic Programming/CoinChange(Minimum Coins).java
+++ Dynamic Programming/CoinChange(Minimum Coins).java
@@ -1,1 +1,25 @@
+class GfG {
 
+    static int minCoinsRecur(int i, int sum, int[] coins) {
+        
+        
+        if (sum == 0) return 0;
+        if (sum < 0 || i == coins.length) return Integer.MAX_VALUE;
+        
+        int take = Integer.MAX_VALUE;
+        
+        
+        if (coins[i] > 0) {
+            take = minCoinsRecur(i, sum - coins[i], coins);
+            if (take != Integer.MAX_VALUE) take++;
+        }
+       
+        int noTake = minCoinsRecur(i + 1, sum, coins);
+        
+        return Math.min(take, noTake);
+    }
+
+    static int minCoins(int[] coins, int sum) {
+        int ans = minCoinsRecur(0, sum, coins);
+        return ans != Integer.MAX_VALUE ? ans : -1;
+    }
