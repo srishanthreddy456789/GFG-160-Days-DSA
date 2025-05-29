0e8bfabbafcc4a36a043408e6d6127bb4e4f7ade
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-05-30 01:21:37 +0530

    Update MinimumJumps.java

--- Dynamic Programming/MinimumJumps.java
+++ Dynamic Programming/MinimumJumps.java
@@ -1,1 +1,30 @@
+class GfG {
+
+    static int minJumpsRecur(int i, int[] arr) {
+        
+        
+        if (i >= arr.length - 1)
+            return 0;
+
+        
+        int ans = Integer.MAX_VALUE;
+        for (int j = i + 1; j <= i + arr[i]; j++) {
+            int val = minJumpsRecur(j, arr);
+            if (val != Integer.MAX_VALUE)
+                ans = Math.min(ans, 1 + val);
+        }
+
+        return ans;
+    }
+
+    static int minJumps(int[] arr) {
+
+        int ans = minJumpsRecur(0, arr);
+        
+        
+        if (ans == Integer.MAX_VALUE) 
+            return -1;
+            
+        return ans;
+    }
 
