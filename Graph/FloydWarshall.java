bbfdfdcd790480c9ae2073cf3bfeadd208a3a210
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:30:11 +0530

    Update FloydWarshall.java

--- Graph/FloydWarshall.java
+++ Graph/FloydWarshall.java
@@ -1,1 +1,19 @@
 
+class GfG {
+  
+    static void floydWarshall(int[][] dist){
+        int V = dist.length;
+
+        for (int k = 0; k < V; k++) {
+
+            for (int i = 0; i < V; i++) {
+
+                for (int j = 0; j < V; j++) {
+
+                    if(dist[i][k] != 1e8 && dist[k][j]!= 1e8)
+                    dist[i][j] = Math.min(dist[i][j],dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+}
