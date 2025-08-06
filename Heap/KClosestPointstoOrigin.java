0b4848c6ef25a9c118f47cad2c902c91df38e319
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:45:04 +0530

    Update KClosestPointstoOrigin.java

--- Heap/KClosestPointstoOrigin.java
+++ Heap/KClosestPointstoOrigin.java
@@ -1,1 +1,38 @@
 
+class GfG {
+
+    static int squaredDis(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+
+    static int[][] kClosest(int[][] points, int k) {
+     
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> squaredDis(b) - squaredDis(a));
+
+        for (int[] point : points) {
+            if (maxHeap.size() < k) {
+              
+                maxHeap.offer(point);
+            } else {
+               
+                if (squaredDis(point) < squaredDis(maxHeap.peek())) {
+                  
+                    maxHeap.poll();
+                    maxHeap.offer(point);
+                }
+            }
+        }
+
+        int[][] res = new int[k][2];
+        int index = 0;
+        while (!maxHeap.isEmpty()) {
+            int[] point = maxHeap.poll();
+            res[index][0] = point[0];
+            res[index][1] = point[1];
+            index++;
+        }
+        return res;
+    }
+
+}
