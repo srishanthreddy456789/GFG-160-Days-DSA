986d982db0f1264ddcc40bafc440fd2d42bc0962
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:32:39 +0530

    Update RottenOranges.java

--- Graph/RottenOranges.java
+++ Graph/RottenOranges.java
@@ -1,1 +1,55 @@
 
+
+public class GfG {
+   
+    private boolean isSafe(int i, int j, int n, int m) {
+        return 0 <= i && i < n && 0 <= j && j < m;
+    }
+
+    public int orangesRotting(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        boolean changed;
+
+        int elapsedTime = 0;
+        int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+        while (true) {
+            changed = false;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+
+                    if (mat[i][j] == elapsedTime + 2) {
+     
+                        for (int[] dir : directions) {
+                            int x = i + dir[0];
+                            int y = j + dir[1];
+             
+                            if (isSafe(x, y, n, m) && mat[x][y] == 1) {
+                                mat[x][y] = mat[i][j] + 1;
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!changed) {
+                break;
+            }
+            elapsedTime++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+  
+                if (mat[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+
+        return elapsedTime;
+    }
+}
