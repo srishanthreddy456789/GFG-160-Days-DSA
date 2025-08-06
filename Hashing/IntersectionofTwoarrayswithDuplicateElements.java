8415dba07dc4227bd6db5650aed2c5a73be5e7e1
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:40:22 +0530

    Update IntersectionofTwoarrayswithDuplicateElements.java

--- Hashing/IntersectionofTwoarrayswithDuplicateElements.java
+++ Hashing/IntersectionofTwoarrayswithDuplicateElements.java
@@ -1,1 +1,25 @@
 
+
+class GfG {
+
+    static ArrayList<Integer> intersect(int[] a, int[] b) {
+      
+
+        HashSet<Integer> sa = new HashSet<>();
+        for (int num : a) {
+            sa.add(num);
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        for (int num : b) {
+          
+            if (sa.contains(num)) {
+                res.add(num);          
+                sa.remove(num);        
+            }
+        }
+
+        return res;
+    }
+}
