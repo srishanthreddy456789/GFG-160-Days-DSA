08e7e31abfa19ef690049f9bdef3931b36ff91b1
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:39:08 +0530

    Update CountSubarrayswithgivenXOR.java

--- Hashing/CountSubarrayswithgivenXOR.java
+++ Hashing/CountSubarrayswithgivenXOR.java
@@ -1,1 +1,27 @@
 
+import java.util.HashMap;
+
+class GfG {
+  
+    static int subarrayXor(int[] arr, int k) {
+        int res = 0;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        int prefXOR = 0;
+
+        for (int val : arr) {
+          
+            prefXOR ^= val;
+
+            res += mp.getOrDefault(prefXOR ^ k, 0);
+
+            if (prefXOR == k)
+                res++;
+            mp.put(prefXOR, mp.getOrDefault(prefXOR, 0) + 1);
+        }
+
+        return res;
+    }
+
+}
