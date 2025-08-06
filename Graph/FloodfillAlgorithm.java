d0bb26ebca047ad9c23371a0715ec877fac3cf3a
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:29:28 +0530

    Update FloodfillAlgorithm.java

--- Graph/FloodfillAlgorithm.java
+++ Graph/FloodfillAlgorithm.java
@@ -1,1 +1,34 @@
 
+
+class GfG {
+
+    static void dfs(int[][] image, int x, int y, 
+                        int oldColor, int newColor){
+
+        if (x < 0 || x >= image.length || 
+            y < 0 || y >= image[0].length || 
+            image[x][y] != oldColor) {
+            return; 
+        }
+
+        image[x][y] = newColor;
+
+        dfs(image, x + 1, y, oldColor, newColor); 
+        dfs(image, x - 1, y, oldColor, newColor);
+        dfs(image, x, y + 1, oldColor, newColor); 
+        dfs(image, x, y - 1, oldColor, newColor); 
+        
+    }
+
+    static int[][] floodFill(int[][] image, int sr, int sc, int newColor){
+
+        if (image[sr][sc] == newColor) {
+            return image;
+        }
+
+        dfs(image, sr, sc, image[sr][sc], newColor);
+
+        return image;
+    }
+
+}
