b1c36d612990035bf40ad47680dd0867c8bb41cd
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:36:02 +0530

    Update GasStation.java

--- Greedy/GasStation.java
+++ Greedy/GasStation.java
@@ -1,1 +1,32 @@
 
+
+class GfG {
+    static int startStation(int[] gas, int[] cost) {
+        int n = gas.length;
+        int startIdx = -1;
+        for (int i = 0; i < n; i++) {
+
+            int currGas = 0;
+            boolean flag = true;
+            for (int j = 0; j < n; j++) {
+                
+                int idx = (i + j) % n;
+                currGas = currGas + gas[idx] - cost[idx];
+                
+                if (currGas < 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+        
+            if (flag) {
+                startIdx = i;
+                break;
+            }
+        }
+        
+        return startIdx;
+    }
+ 
+}
