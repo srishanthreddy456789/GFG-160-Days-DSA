918ba89f0698273d1f9eae0e5160513f0dd53ac6
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:37:50 +0530

    Update MinimumPlatforms.java

--- Greedy/MinimumPlatforms.java
+++ Greedy/MinimumPlatforms.java
@@ -1,1 +1,23 @@
 
+class GfG {
+
+    static int minPlatform(int[] arr, int[] dep) {
+        int n = arr.length;
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j)
+
+                    if (arr[i] >= arr[j] && dep[j] >= arr[i]) {
+                        cnt++;
+                    }
+            }
+
+            res = Math.max(cnt, res);
+        }
+        return res;
+    }
+}
