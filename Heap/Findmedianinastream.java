6ab26faefdda530dca7ff562555afd24ef213c15
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:44:18 +0530

    Update Findmedianinastream.java

--- Heap/Findmedianinastream.java
+++ Heap/Findmedianinastream.java
@@ -1,1 +1,36 @@
 
+class GfG {
+    
+    static ArrayList<Double> getMedian(int[] arr) {
+        
+        PriorityQueue<Integer> leftMaxHeap = new PriorityQueue<>((a, b) -> b - a);
+        
+        PriorityQueue<Integer> rightMinHeap = new PriorityQueue<>();
+        
+        ArrayList<Double> res = new ArrayList<>();
+      
+        for (int i = 0; i < arr.length; i++) {
+            
+            leftMaxHeap.add(arr[i]);
+            
+            int temp = leftMaxHeap.poll();
+            rightMinHeap.add(temp);
+          
+            if (rightMinHeap.size() > leftMaxHeap.size()) {
+                temp = rightMinHeap.poll();
+                leftMaxHeap.add(temp);
+            }
+            
+            double median;
+            if (leftMaxHeap.size() != rightMinHeap.size())
+                median = leftMaxHeap.peek();
+            else
+                median = (leftMaxHeap.peek() + rightMinHeap.peek()) / 2.0;
+            
+            res.add(median);
+        }
+        
+        return res;
+    }
+
+}
