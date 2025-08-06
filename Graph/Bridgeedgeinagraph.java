3117cf57a5c14f2815a56f861c7e721bb53e74c2
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:22:57 +0530

    Update Bridgeedgeinagraph.java

--- Graph/Bridgeedgeinagraph.java
+++ Graph/Bridgeedgeinagraph.java
@@ -1,1 +1,40 @@
 
+
+
+class GfG {
+
+
+    static ArrayList<Integer> bfs(
+        ArrayList<ArrayList<Integer>> adj) {
+        int V = adj.size();
+        
+        int s = 0; 
+        ArrayList<Integer> res = new ArrayList<>();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        boolean[] visited = new boolean[V];
+        
+
+        visited[s] = true;
+        q.add(s);
+        
+
+        while (!q.isEmpty()) {
+            
+
+            int curr = q.poll();
+            res.add(curr);
+            
+
+            for (int x : adj.get(curr)) {
+                if (!visited[x]) {
+                    visited[x] = true;
+                    q.add(x);
+                }
+            }
+        }
+        return res;
+    }
+    
+}
