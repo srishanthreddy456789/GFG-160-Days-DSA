efb14eeaf6a17fe6d945edd599160d657039a653
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:42:22 +0530

    Update SubarrayswithsumK.java

--- Hashing/SubarrayswithsumK.java
+++ Hashing/SubarrayswithsumK.java
@@ -1,1 +1,23 @@
+import java.util.HashSet;
 
+class GfG {
+
+    static boolean twoSum(int[] arr, int target){
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int complement = target - arr[i];
+
+            if (set.contains(complement)) {
+                return true;
+            }
+
+            set.add(arr[i]);
+        }
+
+        return false;
+    }
+
+}
