0cae8226cfcb86f19ec183ac716df35f3f444b5e
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:46:46 +0530

    Update klargestelements.java

--- Heap/klargestelements.java
+++ Heap/klargestelements.java
@@ -1,1 +1,31 @@
 
+import java.util.*;
+
+class GfG {
+    
+    static ArrayList<Integer> kLargest(int[] arr, int k) {
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+
+        for (int i = 0; i < k; i++) {
+            minHeap.add(arr[i]);
+        }
+
+        for (int i = k; i < arr.length; i++) {
+          
+            if (arr[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.add(arr[i]);
+            }
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            res.add(minHeap.poll());
+        }
+
+        Collections.reverse(res);
+        return res;
+    }
+
+}
