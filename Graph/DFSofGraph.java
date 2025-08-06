c5ca7d78b2c2cc5736607753368b7e7eab9f10ba
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:24:37 +0530

    Update DFSofGraph.java

--- Graph/DFSofGraph.java
+++ Graph/DFSofGraph.java
@@ -1,1 +1,39 @@
+import java.util.*;
 
+public class DFSGraph {
+    
+    private static void
+    dfsRec(ArrayList<ArrayList<Integer> > adj,
+           boolean[] visited, int s, ArrayList<Integer> res)
+    {
+        visited[s] = true;
+        res.add(s);
+
+        
+        for (int i : adj.get(s)) {
+            if (!visited[i]) {
+                dfsRec(adj, visited, i, res);
+            }
+        }
+    }
+
+   
+    public static ArrayList<Integer>
+    DFS(ArrayList<ArrayList<Integer> > adj)
+    {
+        boolean[] visited = new boolean[adj.size()];
+        ArrayList<Integer> res = new ArrayList<>();
+        dfsRec(adj, visited, 0, res);
+        return res;
+    }
+
+    
+    public static void
+    addEdge(ArrayList<ArrayList<Integer> > adj, int s,
+            int t)
+    {
+        adj.get(s).add(t);
+        adj.get(t).add(s);
+    }
+
+}
