ac59847df86866b03af095fd14d40af70b75f4d3
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:37:17 +0530

    Update MaximizepartitionsinaString.java

--- Greedy/MaximizepartitionsinaString.java
+++ Greedy/MaximizepartitionsinaString.java
@@ -1,1 +1,25 @@
+public class Main {
+    public static int maxPartition(String s) {
+        int n = s.length();
+        int partitions = 0;
+        boolean[] visited = new boolean[26];
 
+        for (int i = 0; i < n; ++i) {
+            visited[s.charAt(i) - 'a'] = true;
+            boolean isPartition = true;
+
+       
+            for (int j = i + 1; j < n; ++j) {
+                if (visited[s.charAt(j) - 'a']) {
+                    isPartition = false;
+                    break;
+                }
+            }
+
+            if (isPartition)
+                partitions++;
+        }
+
+        return partitions;
+    }
+}
