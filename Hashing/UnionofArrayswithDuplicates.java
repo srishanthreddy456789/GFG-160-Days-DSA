80817636c3116b59821d85a2a8925d4dcfaaaaa6
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-06 15:43:05 +0530

    Update UnionofArrayswithDuplicates.java

--- Hashing/UnionofArrayswithDuplicates.java
+++ Hashing/UnionofArrayswithDuplicates.java
@@ -1,1 +1,20 @@
 
+class GfG {
+    static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        HashSet<Integer> st = new HashSet<>();
+
+        for (int num : a)
+            st.add(num);
+
+        for (int num : b)
+            st.add(num);
+		
+      	ArrayList<Integer> res = new ArrayList<> ();
+      
+        for(int it: st) 
+            res.add(it);
+
+        return res;
+    }
+
+}
