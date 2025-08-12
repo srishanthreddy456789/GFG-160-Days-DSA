3340c9444fa4a6f631690623cb8c82f872d9d97e
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-12 13:18:51 +0530

    Update CountPairswhosesumislessthantarget.java

--- Two Pointer Technique/CountPairswhosesumislessthantarget.java
+++ Two Pointer Technique/CountPairswhosesumislessthantarget.java
@@ -1,1 +1,22 @@
+class GfG {
+    static int countPairs(int[] arr, int target) {
+      
+        Arrays.sort(arr);
+        int left = 0, right = arr.length - 1;
+        int cnt = 0;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum < target) {
+                cnt += right - left;
+                left++;
+            } 
+          	else {
+                right--;
+            }
+        }
+
+        return cnt;
+    }
 
