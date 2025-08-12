89f0cb0faebc564cc374d3164534bfca69d2cf0b
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-12 13:11:37 +0530

    Update Maximumpathsumfromanynode.java

--- Tree/Maximumpathsumfromanynode.java
+++ Tree/Maximumpathsumfromanynode.java
@@ -1,1 +1,22 @@
+class GfG {
+
+    static int maxPathSumUtil(Node root, int[] res) {
+
+        if (root == null) return 0;
+
+        int l = Math.max(0, maxPathSumUtil(root.left, res));
+        int r = Math.max(0, maxPathSumUtil(root.right, res));
+
+        res[0] = Math.max(res[0], l + r + root.data);
+
+        return root.data + Math.max(l, r);
+    }
+
+    static int maxPathSum(Node root) {
+        int[] res = {root.data};
+
+        maxPathSumUtil(root, res);
+
+        return res[0];
+    }
 
