224fe1a2468bb736e60dc0a71feaaa161d3654fe
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-12 13:16:01 +0530

    Create Countalltripletswithgivensuminsortedarray.java

---
+++ Two Pointer Technique/Countalltripletswithgivensuminsortedarray.java
@@ -0,0 +1,45 @@
+class GfG {
+
+    static int countTriplets(int[] arr, int target) {
+        int n = arr.length;
+        int res = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum < target) {
+                    left += 1;
+                }
+
+                else if (sum > target) {
+                    right -= 1;
+                }
+                
+                else if (sum == target) {
+                    int ele1 = arr[left], ele2 = arr[right];
+                    int cnt1 = 0, cnt2 = 0;
+
+                    while (left <= right && arr[left] == ele1) {
+                        left++;
+                        cnt1++;
+                    }
+
+                    while (left <= right && arr[right] == ele2) {
+                        right--;
+                        cnt2++;
+                    }
+                    if (ele1 == ele2)
+                        res += (cnt1 * (cnt1 - 1)) / 2;
+
+                    else
+                        res += (cnt1 * cnt2);
+                }
+            }
+        }
+        return res;
+    }
+}
