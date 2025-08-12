5931820f0bbe49c97a5f4e07dc339cf36bbe4c66
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-12 13:07:55 +0530

    Update ConstructTreefromInorderPreorder.java

--- Tree/ConstructTreefromInorderPreorder.java
+++ Tree/ConstructTreefromInorderPreorder.java
@@ -1,1 +1,59 @@
+class GfG {
+	
+    static void printLevelOrder(Node root) {
+        if (root == null) {
+            System.out.print("N ");
+            return;
+        }
 
+        Queue<Node> qq = new LinkedList<>();
+        qq.add(root);
+        int nonNull = 1;
+
+        while (!qq.isEmpty() && nonNull > 0) {
+            Node curr = qq.poll();
+
+            if (curr == null) {
+                System.out.print("N ");
+                continue;
+            }
+            nonNull--;
+
+            System.out.print(curr.data + " ");
+            qq.add(curr.left);
+            qq.add(curr.right);
+            if (curr.left != null)
+                nonNull++;
+            if (curr.right != null)
+                nonNull++;
+        }
+    }
+
+    static Node buildTreeRecur(Map<Integer, Integer> mp, int[] preorder, int[] preIndex, int left, int right) {
+
+        if (left > right)
+            return null;
+
+        int rootVal = preorder[preIndex[0]];
+        preIndex[0]++;
+
+        Node root = new Node(rootVal);
+
+        int index = mp.get(rootVal);
+
+        root.left = buildTreeRecur(mp, preorder, preIndex, left, index - 1);
+        root.right = buildTreeRecur(mp, preorder, preIndex, index + 1, right);
+
+        return root;
+    }
+
+    static Node buildTree(int[] inorder, int[] preorder) {
+
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++)
+            mp.put(inorder[i], i);
+
+        int[] preIndex = {0};
+        return buildTreeRecur(mp, preorder, preIndex, 0, inorder.length - 1);
+    }
+}
