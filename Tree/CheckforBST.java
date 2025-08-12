125a2bbc86be2ff96497b0b258a1e89f5b03658c
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-12 13:06:27 +0530

    Update CheckforBST.java

--- Tree/CheckforBST.java
+++ Tree/CheckforBST.java
@@ -1,1 +1,44 @@
+class GfG {
 
+    static boolean isBST(Node root) {
+        Node curr = root;
+        Node pre;
+        int prevValue = Integer.MIN_VALUE;
+
+        while (curr != null) {
+            if (curr.left == null) {
+              
+                if (curr.data <= prevValue) {
+                  
+                    return false;
+                }
+                prevValue = curr.data;
+                curr = curr.right;
+            } else {
+              
+                pre = curr.left;
+                while (pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+
+                if (pre.right == null) {
+                  
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                  
+                    pre.right = null;
+
+                    if (curr.data <= prevValue) {
+                      
+                        return false;
+                    }
+                    prevValue = curr.data;
+                    curr = curr.right;
+                }
+            }
+        }
+
+        return true;
+    }
+}
