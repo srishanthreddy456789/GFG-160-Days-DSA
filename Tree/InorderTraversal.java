ffd029b654169e1f128c82f4ca97fa62ffaba39c
Srishanth reddy <151022057+srishanthreddy456789@users.noreply.github.com>
2025-08-12 13:09:05 +0530

    Update InorderTraversal.java

--- Tree/InorderTraversal.java
+++ Tree/InorderTraversal.java
@@ -1,1 +1,36 @@
+class GfG {
+
+    static ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Node curr = root;
+
+        while (curr != null) {
+            if (curr.left == null) {
+              
+                res.add(curr.data);
+                curr = curr.right;
+            } 
+            else {
+              
+                Node prev = curr.left;
+                while (prev.right != null && 
+                                   prev.right != curr) {
+                    prev = prev.right;
+                }
+
+                if (prev.right == null) {
+                    prev.right = curr;
+                    curr = curr.left;
+                } 
+                else {
+                  
+                    prev.right = null;
+                    res.add(curr.data);
+                    curr = curr.right;
+                }
+            }
+        }
+        return res;
+    }
+}
 
